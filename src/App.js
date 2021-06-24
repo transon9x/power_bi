9de81bd7527decad9200b6c0cc9a057882952208
unknown <BIDVAdmin@h40des02086.bidvbank.bidv.com>
2021-06-24 13:44:51 +0700

    format

--- src/App.js
+++ src/App.js
@@ -1,110 +1,118 @@
-// import logo from './logo.svg';
-import './App.css';
-import { PowerBIEmbed } from 'powerbi-client-react';
-import {models} from 'powerbi-client'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import {isMobile} from 'react-device-detect';
+import "./App.css";
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from "powerbi-client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { isMobile } from "react-device-detect";
 
+function App() {
+  const [data, setData] = useState([]);
 
-function App() {  
-  const [data, setData] = useState([])
+  useEffect(async () => {
+    const list = await axios({
+      url: "http://192.168.1.96:9999/get_data",
+      method: "GET",
+    });
+    setData(list.data);
+  }, []);
 
-  useEffect(async()=> {
-    const list= await axios({
-      url: 'http://192.168.1.96:9999/get_data',
-      method: 'GET'
-    }
-    )
-    setData(list.data)
-  },[])
-
-  if (isMobile){
+  if (isMobile) {
     return (
       <PowerBIEmbed
-          embedConfig = {{
-            type: 'report',  
-            id: data.id_report,
-            embedUrl: data.url_em,
-            accessToken: data.token,
-            tokenType: models.TokenType.Aad,
-            settings: {
-              panes: {
-                filters: {
-                  expanded: false,
-                  visible: false
-                }
-              
-                
+        embedConfig={{
+          type: "report",
+          id: data.id_report,
+          embedUrl: data.url_em,
+          accessToken: data.token,
+          tokenType: models.TokenType.Aad,
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
               },
-              // layoutType: models.LayoutType.MobileLandscape,
-              // layoutType: models.LayoutType.MobilePortrait,
-              background: models.BackgroundType.Transparent,
-              
-            
-              
-            }
-          }}
-  
-          eventHandlers = { 
-            new Map([
-              ['loaded', function () {console.log('Report loaded');}],
-              ['rendered', function () {console.log('Report rendered');}],
-              ['error', function (event) {console.log(event.detail);}]
-            ])
-          }
-            
-          cssClassName = { "Embed-container" }
-  
-          getEmbeddedComponent = { (embeddedReport) => {
-            window.report = embeddedReport;
-          }}
+            },
+            // layoutType: models.LayoutType.MobileLandscape,
+            // layoutType: models.LayoutType.MobilePortrait,
+            background: models.BackgroundType.Transparent,
+          },
+        }}
+        eventHandlers={
+          new Map([
+            [
+              "loaded",
+              function () {
+                console.log("Report loaded");
+              },
+            ],
+            [
+              "rendered",
+              function () {
+                console.log("Report rendered");
+              },
+            ],
+            [
+              "error",
+              function (event) {
+                console.log(event.detail);
+              },
+            ],
+          ])
+        }
+        cssClassName={"Embed-container"}
+        getEmbeddedComponent={(embeddedReport) => {
+          window.report = embeddedReport;
+        }}
       />
     );
-  } else{
+  } else {
     return (
       <PowerBIEmbed
-          embedConfig = {{
-            type: 'report',  
-            id: data.id_report,
-            embedUrl: data.url_em,
-            accessToken: data.token,
-            tokenType: models.TokenType.Aad,
-            settings: {
-              panes: {
-                filters: {
-                  expanded: false,
-                  visible: false
-                }
+        embedConfig={{
+          type: "report",
+          id: data.id_report,
+          embedUrl: data.url_em,
+          accessToken: data.token,
+          tokenType: models.TokenType.Aad,
+          settings: {
+            panes: {
+              filters: {
+                expanded: false,
+                visible: false,
               },
-              
+            },
+          },
+          displayOption: models.DisplayOption.FitToPage,
+        }}
+        eventHandlers={
+          new Map([
+            [
+              "loaded",
+              function () {
+                console.log("Report loaded");
               },
-              displayOption: models.DisplayOption.FitToPage
-              
-  
-              
-              // background: models.BackgroundType.Transparent,
-            }
-          }
-  
-          eventHandlers = { 
-            new Map([
-              ['loaded', function () {console.log('Report loaded');}],
-              ['rendered', function () {console.log('Report rendered');}],
-              ['error', function (event) {console.log(event.detail);}]
-            ])
-          }
-            
-          cssClassName = { "Embed-container-web" }
-  
-          getEmbeddedComponent = { (embeddedReport) => {
-            window.report = embeddedReport;
-          }}
+            ],
+            [
+              "rendered",
+              function () {
+                console.log("Report rendered");
+              },
+            ],
+            [
+              "error",
+              function (event) {
+                console.log(event.detail);
+              },
+            ],
+          ])
+        }
+        cssClassName={"Embed-container-web"}
+        getEmbeddedComponent={(embeddedReport) => {
+          window.report = embeddedReport;
+        }}
       />
     );
   }
-
-  
-      }
+}
 
 export default App;
